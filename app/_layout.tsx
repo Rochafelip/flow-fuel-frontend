eabdd285709fbc514aa4c3bc68ba876b5d8092bd
Felipe Rocha <rocha.felipe98@gmail.com>
2026-02-17 19:57:39 -0300

    subindo a primeira versao do app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,51 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { AuthProvider, useAuth } from "../context/AuthContext";
+import { VehicleProvider, useVehicle } from "../context/VehicleContext";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+function AppContent() {
+  const { token, loading } = useAuth();
+  const { activeVehicle, loadingVehicle } = useVehicle();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (loading || loadingVehicle) return;
+
+    if (!token) {
+      router.replace("/login");
+    } else if (!activeVehicle) {
+      router.replace("/select-vehicle");
+    } else {
+      router.replace("/"); 
+    }
+  }, [token, activeVehicle, loading, loadingVehicle]);
+
+
+  if (loading || loadingVehicle) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <AuthProvider>
+      <VehicleProvider>
+        <AppContent />
+      </VehicleProvider>
+    </AuthProvider>
   );
 }
